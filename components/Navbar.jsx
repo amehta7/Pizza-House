f0fdb5b83bc7df3653dad0b317e44c077e0c7474
amehta7 <amimehta2811@gmail.com>
2023-10-11 17:04:41 -0700

    added models

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 import styles from '../styles/Navbar.module.css'
+import { FaBars } from 'react-icons/fa'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -41,6 +45,62 @@ const Navbar = () => {
           <div className={styles.counter}>2</div>
         </div>
       </div>
+
+      <div className={styles.MobileIcon}>
+        <FaBars color='white' onClick={() => setOpen(!open)} />
+      </div>
+      {open && (
+        <ul className={styles.MobileMenu} open={open}>
+          <li
+            className={styles.listItem}
+            onClick={() => {
+              setOpen(!open)
+            }}
+          >
+            Home
+          </li>
+          <li
+            className={styles.listItem}
+            onClick={() => {
+              setOpen(!open)
+            }}
+          >
+            Products
+          </li>
+          <li
+            className={styles.listItem}
+            onClick={() => {
+              setOpen(!open)
+            }}
+          >
+            Menu
+          </li>
+          <li
+            className={styles.listItem}
+            onClick={() => {
+              setOpen(!open)
+            }}
+          >
+            Events
+          </li>
+          <li
+            className={styles.listItem}
+            onClick={() => {
+              setOpen(!open)
+            }}
+          >
+            Blog
+          </li>
+          <li
+            className={styles.listItem}
+            onClick={() => {
+              setOpen(!open)
+            }}
+          >
+            Contact
+          </li>
+        </ul>
+      )}
     </div>
   )
 }
